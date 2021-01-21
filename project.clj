68b79e57f9d8400cef22dd3be566ec2460b58f72
Eric Dallo <ericdallo06@hotmail.com>
2021-01-21 20:11:46 -0300

    Use deps.edn

--- project.clj
+++ project.clj
@@ -1,12 +1,24 @@
+(require '[clojure.edn :as edn])
+
+(def +deps+ (-> "deps.edn" slurp edn/read-string))
+
+(defn deps->vec [deps]
+  (keep (fn [[dep {:keys [:mvn/version exclusions]}]]
+          (when version
+            (cond-> [dep version]
+              exclusions (conj :exclusions exclusions))))
+        deps))
+
+(def dependencies
+  (deps->vec (:deps +deps+)))
+
+
 (defproject clojure-sample "0.1.0-SNAPSHOT"
+  :dependencies ~dependencies
   :description "Clojure sample"
   :url "http://github/com/ericdallo/clojure-sample"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [nubank/matcher-combinators "3.1.2"]
-                 [org.clojure/core.async "1.3.610"]
-                 [prismatic/schema "1.1.12"]]
   :main ^:skip-aot clojure-sample.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
